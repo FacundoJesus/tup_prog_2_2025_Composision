7df9f1808a83a5565771f34c4e551cba6a312d23
FacuJesus <facundojesus10@hotmail.com>
2025-08-25 08:56:41 -0300

    Tarjeta - Persona / Composicion

---
+++ Ejercicios/Ejercicio1/Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Persona
+    {
+        private string nombre;
+        private int dni;
+        public DateTime FechaNac;
+        
+        public Persona(string nombre, int dni)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+
+        public string Datos()
+        {
+            return $"DNI: {this.dni} - Nombre: {this.nombre} - {this.FechaNac}";
+        }
+        
+
+    }
+}
---
+++ Ejercicios/Ejercicio1/Models/Tarjeta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class Tarjeta
+    {
+        public int Nro {  get; set; }
+
+        public Persona Dueño;
+
+        public DateTime FechaIngreso;
+        public DateTime FechaSalida;
+
+        public double Saldo;
+
+        public Tarjeta(int numero, string nombre, int dni)
+        {
+            Dueño = new Persona(nombre,dni);
+            this.Nro = numero;
+            this.Saldo = 0;
+            this.FechaIngreso = DateTime.Now;
+            
+        }
+
+        public bool CargarJuego(double monto)
+        {
+            bool pudoJugar  = false;
+
+            if (this.Saldo >= monto) {
+                Saldo -= monto;
+                pudoJugar = true;
+            }
+            return pudoJugar;
+        }
+    }
+
+}
