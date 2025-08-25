8736749343f405e66c41c0e30d9cc3ff99444236
FacuJesus <facundojesus10@hotmail.com>
2025-08-25 08:56:59 -0300

    Cliente - Cuenta / Composicion

---
+++ Ejercicios/Ejercicio2/Models/Cliente.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Cliente
+    {
+        public int Nro {  get; set; }
+        public long Cuit {  get; set; }
+
+        List <Cuenta> cuentas = new List<Cuenta> ();
+
+        public Cliente (long cuit)
+        {
+            this.Cuit = cuit;
+
+        }
+
+        public bool CrearCuenta(string tipo, string moneda)
+        {
+            Cuenta cuenta = new Cuenta(this.Nro,moneda);
+
+            Cuenta cuentaExistente = VerCuenta(cuenta.Nro);
+            if(cuentaExistente == null) {
+                cuentas.Add(cuenta);
+                Nro++;
+                return true;
+            }
+            return false;
+            
+        }
+
+        public Cuenta VerCuenta(int numero)
+        {
+            Cuenta cuenta = null;
+            int i = 0;
+            int pos = -1;
+
+            while(pos==-1 && i < cuentas.Count)
+            {
+                if (cuentas[i].Nro == numero)
+                {
+                    pos = i;
+                }
+                i++;
+            }
+            if (pos != -1)
+            {
+                cuenta = cuentas[pos];
+            }
+            return cuenta;
+        }
+
+        public List<Cuenta> ListaDeCuentas()
+        {
+            return cuentas;
+        }
+    }
+}
---
+++ Ejercicios/Ejercicio2/Models/Cuenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Models
+{
+    public class Cuenta
+    {
+        public int Nro {  get; private set; }
+        public string Moneda { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Cuenta(int nro, string moneda)
+        {
+            this.Nro = nro;
+            this.Moneda = moneda;
+        }
+
+
+    }
+}
